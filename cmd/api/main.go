32239c240525334a9213b2d29cf49214d0dde0a8
athif <athifirshad@hotmail.com>
2024-09-14 09:10:04 +0530

    initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"runtime/debug"
+	"sync"
+
+	"athifirshad.com/bettergram/internal/database"
+	"athifirshad.com/bettergram/internal/version"
+
+	"github.com/lmittmann/tint"
+)
+
+func main() {
+	logger := slog.New(tint.NewHandler(os.Stdout, &tint.Options{Level: slog.LevelDebug}))
+
+	err := run(logger)
+	if err != nil {
+		trace := string(debug.Stack())
+		logger.Error(err.Error(), "trace", trace)
+		os.Exit(1)
+	}
+}
+
+type config struct {
+	baseURL  string
+	httpPort int
+	db       struct {
+		dsn string
+	}
+}
+
+type application struct {
+	config config
+	db     *database.DB
+	logger *slog.Logger
+	wg     sync.WaitGroup
+}
+
+func run(logger *slog.Logger) error {
+	var cfg config
+
+	flag.StringVar(&cfg.baseURL, "base-url", "http://localhost:4444", "base URL for the application")
+	flag.IntVar(&cfg.httpPort, "http-port", 4444, "port to listen on for HTTP requests")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "user:pass@localhost:5432/db", "postgreSQL DSN")
+
+	showVersion := flag.Bool("version", false, "display version and exit")
+
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("version: %s\n", version.Get())
+		return nil
+	}
+
+	db, err := database.New(cfg.db.dsn)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	app := &application{
+		config: cfg,
+		db:     db,
+		logger: logger,
+	}
+
+	return app.serveHTTP()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module athifirshad.com/bettergram
+
+go 1.23.0
